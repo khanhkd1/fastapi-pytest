93ad84f3c9779062e9e1e28b0393295be0a52e74
KhanhKD-seta <khanhkieu167@gmail.com>
2023-02-13 09:25:49 +0700

    go to goc nhin and click first author

--- selenium/test.py
+++ selenium/test.py
@@ -5,26 +5,41 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 action = ActionChains(driver)
 driver.maximize_window()
 
 # Go to main site
-driver.get("https://vnexpress.net/")
-time.sleep(10)
-
-# Click to the first post
-css_first_post = "article.article-topstory .thumb-art > a"
-element_selector = driver.find_elements(By.CSS_SELECTOR, css_first_post)[0]
-action.move_to_element(element_selector)
-action.click()
-action.perform()
-time.sleep(10)
+# driver.get("https://vnexpress.net/")
+# time.sleep(10)
+#
+# # Click to the first post
+# css_first_post = "article.article-topstory .thumb-art > a"
+# element_selector = driver.find_elements(By.CSS_SELECTOR, css_first_post)[0]
+# action.move_to_element(element_selector)
+# action.click()
+# action.perform()
+# time.sleep(10)
 
 # Back to main site
 driver.get("https://vnexpress.net/")
 time.sleep(10)
 
+# Click to Goc Nhin
 css_goc_nhin = "li.gocnhin .thumb-art > a"
+element_selector = driver.find_elements(By.CSS_SELECTOR, '#wrap-main-nav > nav > ul > li.gocnhin')[0]
+action.move_to_element(element_selector)
+action.click()
+action.perform()
+time.sleep(10)
+
+# Click to first author
+css_author = "ul.list-author-home > li"
+element_selector = driver.find_elements(By.CSS_SELECTOR, 'ul.list-author-home > li')[0]
+action.move_to_element(element_selector)
+action.click()
+action.perform()
+time.sleep(10)
+
 
 driver.close()
